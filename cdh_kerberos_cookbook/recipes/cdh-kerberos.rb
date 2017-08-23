d819e83566582a4eb705ba30eb31b618d72efe65
Evgenii Seliavka <evg.selyavka@gmail.com>
2017-08-23 15:47:00 -0400

    add keytab for ubuntu user

--- cdh_kerberos_cookbook/recipes/cdh-kerberos.rb
+++ cdh_kerberos_cookbook/recipes/cdh-kerberos.rb
@@ -4,18 +4,35 @@
 #
 # Copyright:: 2017, The Authors, All Rights Reserved.
 
-%w(hdfs mapred yarn HTTP).each do |user|
+user_ubuntu = 'ubuntu'
+file_ubuntu = "/home/#{user_ubuntu}/#{user_ubuntu}.keytab"
+
+users=%w(hdfs mapred yarn HTTP)
+users.push(user_ubuntu)
+
+users.each do |user|
    principal = "#{user}/#{node['cdh_kerberos_cookbook']['kerberos']['domain']}@#{node['cdh_kerberos_cookbook']['kerberos']['realm']}"
     bash 'create_principals' do
         user 'root'
         code <<-EOH
         kadmin.local -q "addprinc -randkey #{principal}"
         EOH
-        not_if 'kadmin.local -q "getprinc -terse #{principal}" 2>/dev/null | tail -1 | awk \'{gsub(/"/, "", $1); if ($1 == "#{principal}") {exit 0} else {exit 1}}\''
+        not_if "kadmin.local -q 'getprinc -terse #{principal}' 2>/dev/null | tail -1 | awk '{gsub(/\"/, \"\", $1); if ($1 == \"#{principal}\") {exit 0} else {exit 1}}'"
     end
 end
 
-%w(hdfs mapred yarn).each do |user|
+bash "create_keytab_for_#{user_ubuntu}" do
+  user 'root'
+  code <<-EOH
+  kadmin.local -q "xst -norandkey -k #{file_ubuntu} #{user_ubuntu}/#{node['cdh_kerberos_cookbook']['kerberos']['domain']} HTTP/#{node['cdh_kerberos_cookbook']['kerberos']['domain']}" && chown #{user_ubuntu}:#{user_ubuntu} #{file_ubuntu} && chmod 0400 #{file_ubuntu}
+  EOH
+  not_if { ::File.exists?(file_ubuntu) }
+end
+
+users.pop
+users.pop
+
+users.each do |user|
     file = "/etc/hadoop/conf/#{user}.keytab"
     bash 'create_keytabs' do
         user 'root'
@@ -25,6 +42,7 @@ end
         not_if { ::File.exists?(file) }
     end
 end
+
 
 cookbook_file '/etc/hadoop/conf/core-site.xml' do
     source 'core-site.xml'
@@ -68,6 +86,7 @@ bash 'generate_certs' do
             -alias #{node['cdh_kerberos_cookbook']['kerberos']['domain']} -storepass #{node['cdh_kerberos_cookbook']['tls']['keystore']['passwd']} | \
     keytool -import -keystore #{node['cdh_kerberos_cookbook']['tls']['truststore']['file']} -alias #{node['cdh_kerberos_cookbook']['kerberos']['domain']} -noprompt -storepass #{node['cdh_kerberos_cookbook']['tls']['truststore']['passwd']}
     EOH
+    not_if { ::File.exists?(node['cdh_kerberos_cookbook']['tls']['truststore']['file']) && ::File.exists?(node['cdh_kerberos_cookbook']['tls']['keystore']['file']) }
 end
 
 bash 'format_nn' do
@@ -93,7 +112,14 @@ end
 bash 'kinit' do
     user 'hdfs'
     code <<-EOH
-    kinit -k -t /etc/hadoop/conf/hdfs.keytab hdfs/#{node['cdh_kerberos_cookbook']['kerberos']['domain']}@#{node['cdh_kerberos_cookbook']['kerberos']['realm']}
+    kinit -k -t /etc/hadoop/conf/hdfs.keytab hdfs/#{node['cdh_kerberos_cookbook']['kerberos']['domain']}@#{node['cdh_kerberos_cookbook']['kerberos']['realm']} && hdfs dfs -mkdir -p /user/hdfs /user/#{user_ubuntu}
+    EOH
+end
+
+bash "kinit_#{user_ubuntu}" do
+    user 'ubuntu'
+    code <<-EOH
+    kinit -k -t #{file_ubuntu} #{user_ubuntu}/#{node['cdh_kerberos_cookbook']['kerberos']['domain']}@#{node['cdh_kerberos_cookbook']['kerberos']['realm']}
     EOH
 end
 
@@ -102,5 +128,5 @@ bash 'init_hdfs' do
     code <<-EOH
     /usr/lib/hadoop/libexec/init-hdfs.sh
     EOH
-    not_if 'su -s /bin/bash hdfs -c "/usr/bin/hadoop fs -test -e /user"'
+    not_if 'su -s /bin/bash hdfs -c "/usr/bin/hadoop fs -test -e /tmp"'
 end
